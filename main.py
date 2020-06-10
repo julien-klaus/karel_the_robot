f7febeb6ad1cd3a84ab4e8faea1acccfd30271bb
Julien Klaus <julien.klaus@uni-jena.de>
2020-06-10 13:02:16 +0200

    * interpret the ast

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 
 import os
 from copy import deepcopy
+import time
+import sys
+import subprocess
 
 from compiler.lexer import Lexer
 from compiler.parser import Parser, SymbolTable
 from compiler.ast import CallBlock, SequenceBlock, CommandBlock, ConditionBlock
 
 def parse_test_case_and_get_results(test_case):
+    print(test_case, end="\n")
     results = []
     # we know exactly the structure of the document
     with open(os.path.join("test", test_case), "r") as file:
@@ -29,6 +33,7 @@ def parse_test_case_and_get_results(test_case):
             x, y, o = file.readline()[:-1].split(" ")
             p = Parser(file.readline()[:-1])
             ast = p.program()
+            print(f"Start: {x}, {y}, {o}.")
             results.append(interpret_labyrinth_for_program(labyrinth, int(x)-1, int(y)-1, o, ast, proc_defs))
     return results
 
@@ -40,9 +45,7 @@ def interpret_labyrinth_for_program(labyrinth, start_x, start_y, start_orientati
     y = start_y
     cur_lab[x][y] = start_orientation
     cur_node = program
-    print("Start:", start_x+1, start_y+1, start_orientation)
     lab, x, y, o = interpret_command_and_update_labyrinth(cur_node, cur_lab, proc_definitions, x, y, start_orientation)
-    print("Result:", x+1, y+1, o)
     return x+1, y+1, o
 
 def interpret_command_and_update_labyrinth(node, labyrinth, proc_defs, x, y, o):
@@ -59,22 +62,22 @@ def interpret_command_and_update_labyrinth(node, labyrinth, proc_defs, x, y, o):
                 x, y = check_if_move_is_possible(labyrinth, x, y, o)
                 # update position
                 labyrinth[x][y] = o
-                print(labyrinth)
+                print_labyrinth(labyrinth)
             elif node.name == "l":
                 o = update_orientation(o)
                 # update orientation
                 labyrinth[x][y] = o
-                print(labyrinth)
+                print_labyrinth(labyrinth)
         elif isinstance(node, CallBlock):
             node_list.insert(0, proc_defs.get_program(node.name))
         elif isinstance(node, ConditionBlock):
             if node.name == "i":
-                if node.condition == o or check_barrier(labyrinth, x, y, o):
+                if node.condition == o or (check_barrier(labyrinth, x, y, o) and node.condition == "b"):
                     node_list.insert(0, node.true_case)
                 else:
                     node_list.insert(0, node.false_case)
             elif node.name == "u":
-                if node.condition != o and not check_barrier(labyrinth, x, y, o):
+                if (node.condition != o and node.condition != "b") or (node.condition == "b" and not check_barrier(labyrinth, x, y, o)):
                     node_list.insert(0, node)
                     node_list.insert(0, node.true_case)
         elif not node:
@@ -82,6 +85,14 @@ def interpret_command_and_update_labyrinth(node, labyrinth, proc_defs, x, y, o):
         else:
             raise Exception(f"Node type {node.__class__} not known.")
     return labyrinth, x, y, o
+
+def print_labyrinth(labyrinth, pause=0.1, verbose=False):
+    if verbose:
+        for line in labyrinth:
+            for item in line:
+                print(item, end="")
+            print()
+        print()
 
 def check_barrier(labyrinth, x, y, o):
     n = len(labyrinth)-1
@@ -155,9 +166,6 @@ def check_if_move_is_possible(labyrinth, x, y, o):
 
 
 
-
-
-
 if __name__ == "__main__":
     test_files = []
     for file in os.listdir("test"):
@@ -169,10 +177,9 @@ if __name__ == "__main__":
             with open(os.path.join("test", f"{file[:-3]}.ans"), "r") as answer_file:
                 answers.append(tuple(answer_file.readline()[:-1].split(" ")))
             for result, answer in zip(results, answers):
-                print(result, answer)
                 if not (result[0] == int(answer[0]) and result[1] == int(answer[1]) and result[2] == answer[2]):
-                    print("Test case not correct.")
-            break
-
-
+                    print(result, answer)
+                    raise Exception("Test case not correct.", file)
+                else:
+                    print("correct")
 
